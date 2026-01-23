5a8f31aa0203ba41ccb3ff33932a6bd6c3039d2b
Brandon Kish <bkish@asu.edu>
2026-01-23 08:54:44 -0500

    Revert "Add AI-Toolkit Log Parser"
    This reverts commit 3136865a639bf5ee343872bfe06302a553d71afd.

--- AI-Toolkit Log Parser/AI-Toolkit Log Parser/LogEntry.cs
+++
@@ -1,8 +0,0 @@
-﻿public class LogEntry
-{
-    public string? FileName { get; set; }  // Make FileName nullable
-    public float Loss { get; set; }
-    public int Step { get; set; }
-
-    public string Display => $"{FileName} - Loss: {Loss} - Step: {Step}";
-}
